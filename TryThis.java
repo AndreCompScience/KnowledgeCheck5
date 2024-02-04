6d7fe50cf535012483376c7c8157cb3909215a7a
Andre <gibranayala798@gmail.com>
2024-02-04 17:55:06 -0400

    Finished

---
+++ TryThis.java
@@ -0,0 +1,6 @@
+class TryThis {
+    public static void main(String[] args) {
+        short payCheck = 53000; // an integer literal
+        System.out.println("The variable contains" + payCheck);
+    }
+}
